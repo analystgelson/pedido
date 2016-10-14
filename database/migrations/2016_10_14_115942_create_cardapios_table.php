45c0893125afda167c473664e8c620bd64a9c4d4
Gelson Moraes <analyst.gelson@gmail.com>
2016-10-14 08:01:01 -0400

    criado a tabela de cardápio

---
+++ database/migrations/2016_10_14_115942_create_cardapios_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCardapiosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('cardapio', function (Blueprint $table) {
+            $table->increments('id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('cardapio');
+    }
+}
